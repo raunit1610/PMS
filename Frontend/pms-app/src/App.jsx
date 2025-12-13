f2d1fc0c89b2db54873dd615b08c28a130196aee
Raunit Raj <raunitraj2018@gmail.com>
2025-12-13 11:54:24 +0530

    removed tailwind

--- Frontend/pms-app/src/App.jsx
+++ Frontend/pms-app/src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home/Home';
 
 function App() {
-
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
